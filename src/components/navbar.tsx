b3d95483e904550b49cf61a82d9d19423a227e50
fajarkusumaa <fajarkusuma46@gmail.com>
2023-09-16 11:17:26 +0700

    fixing navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,72 +4,79 @@ import { Link, NavLink } from "react-router-dom";
 import { useScroll } from "framer-motion";
 
 interface MenuListItem {
-  label: string;
-  path: string;
+    label: string;
+    path: string;
 }
 
 const Navbar: FC = () => {
-  const menuListItem: MenuListItem[] = [
-    {
-      label: "Works",
-      path: "/works",
-    },
-    {
-      label: "Careers",
-      path: "/careers",
-    },
-    {
-      label: "About Us",
-      path: "/about",
-    },
-    {
-      label: "Structure",
-      path: "/structure",
-    },
-  ];
-
-  const { scrollY } = useScroll();
-  const navContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (navContainer.current) {
-        if (scrollY.get() > 50) {
-          navContainer.current.classList.add("navShadow");
-        } else {
-          navContainer.current.classList.remove("navShadow");
+    const menuListItem: MenuListItem[] = [
+        {
+            label: "Works",
+            path: "/works"
+        },
+        {
+            label: "Careers",
+            path: "/careers"
+        },
+        {
+            label: "About Us",
+            path: "/about"
+        },
+        {
+            label: "Structure",
+            path: "/structure"
         }
-      }
-    });
-  }, []);
+    ];
 
-  return (
-    <nav ref={navContainer} className="bg-white py-4 transition-all">
-      <div className="container mx-auto flex justify-between">
-        <Link to="/">
-          <img src={PhicosLogo} alt="phicos logo" className="h-12" />
-        </Link>
+    const { scrollY } = useScroll();
+    const navContainer = useRef<HTMLDivElement>(null);
 
-        <div className="flex items-center flex-1 justify-end">
-          <ul className="flex gap-4 text-slate-700">
-            {menuListItem.map((item) => (
-              <NavLink
-                to={item.path}
-                key={item.label}
-                className={({ isActive }) => {
-                  return isActive
-                    ? "border-b-2 border-gray-200 text-slate-900"
-                    : "";
-                }}
-              >
-                {item.label}
-              </NavLink>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (navContainer.current) {
+                if (scrollY.get() > 50) {
+                    navContainer.current.classList.add("navShadow");
+                } else {
+                    navContainer.current.classList.remove("navShadow");
+                }
+            }
+        });
+    }, []);
+
+    return (
+        <nav ref={navContainer} className="bg-white py-5 transition-all">
+            <div className="container mx-auto flex justify-between">
+                <Link to="/">
+                    <img src={PhicosLogo} alt="phicos logo" className="h-12" />
+                </Link>
+
+                <div className="flex items-center flex-1 justify-center">
+                    <ul className="flex gap-5 text-slate-700">
+                        {menuListItem.map((item) => (
+                            <NavLink
+                                to={item.path}
+                                key={item.label}
+                                className={({ isActive }) => {
+                                    return isActive
+                                        ? "border-b-2 border-gray-200 text-slate-900"
+                                        : "";
+                                }}
+                            >
+                                {item.label}
+                            </NavLink>
+                        ))}
+                    </ul>
+                </div>
+
+                <button
+                    id=""
+                    className="py-2 px-4 text-white bg-primary rounded-lg"
+                >
+                    Contact Us
+                </button>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
